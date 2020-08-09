eab793669f8ec6192a95749d98339f562dad91f2
timtech-wiz <60903808+timtech-wiz@users.noreply.github.com>
2020-08-09 19:44:28 +0100

    Add files via upload

---
+++ login_system/admin.php
@@ -0,0 +1,8 @@
+<?php include "includes/header.php" ?>
+<?php include "includes/nav.php" ?>
+
+<div class="well">
+    <h2>Admin Area</h2>
+    <p><?php echo $_SESSION['username']; ?></p>
+</div>
+<?php include "includes/footer.php" ?>
---
+++ login_system/config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+// Database Params
+
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "54321");
+define("DB_NAME", "fablog");
+
+// DEFINE PATH
+
+define("ROOT_PATH", "http://localhost/projects/login_system");
